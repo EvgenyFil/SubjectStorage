f003b20b8eea4652da5e32322dc1fcc98e5694db
Evgeny <filugin@gmail.com>
2018-09-05 13:46:50 +0300

    Minimal functional of DataStorage
    Realized main methods of DataStorage and Main()

--- Program.cs
+++ Program.cs
@@ -9,10 +9,10 @@ namespace SubjectsStorage
     public interface IDataStorage
     {
         void PutSubject(Subject subject);
+        // TODO: To think about to deprecate this method
         void PutSubjects(IEnumerable<Subject> subjects);
         IEnumerable<Subject> GetAllSubjects();
     }
-
     // TODO: To think about using structure instead a class
     public class Subject
     {
@@ -29,21 +29,19 @@ namespace SubjectsStorage
 
         // TODO: To think about right way to throw exceptions
         // TODO: To think about using only strings as arguments
+        // TODO: To think about to deprecate this
         public static Subject CreateSubject(string name, string surname,
             string patronymic, long passportNumber, DateTime birthday)
         {
+            throw new NotImplementedException();
+            /*
             Subject newSubject = null;
             string errorText = null;
-            if (CheckSubjectValues(name, surname, patronymic,
-                passportNumber, birthday, out newSubject, out errorText))
+            if (CheckValues(name, surname, patronymic,
+                passportNumber, birthday, out errorText))
             {
-                return new Subject{
-                    Name = name,
-                    Surname = surname,
-                    Patronymic = patronymic,
-                    PassportNumber = passportNumber,
-                    Birthday = birthday
-                };
+                return new Subject(name, surname, patronymic,
+                    passportNumber, birthday);
             }
             else
             {
@@ -51,88 +49,87 @@ namespace SubjectsStorage
                 exception.Data.Add("Bad input", errorText);
                 throw exception;
             }
+            */
 
         }
-
-        private static bool CheckSubjectValues(string name, string surname,
-            string patronymic, long passportNumber, DateTime birthday,
-            out string errorString)
+        public static Subject CreateSubject(string name, string surname,
+            string patronymic, string passportNumber, string birthday)
         {
-            errorString = null;
-            if (!CheckSubjectName(name))
+            return new Subject(name, surname, patronymic, passportNumber, birthday);
+        }
+
+        private static bool CheckValues(string name, string surname,
+            string patronymic, long passportNumber, DateTime birthday,
+            ref List<string> errorMessages)
+        {
+            if (errorMessages == null)
             {
-                errorString = "Incorrect first name";
+                errorMessages = new List<string>(4);
             }
-            else if (!CheckSubjectName(surname))
+            if (!CheckStringValue(name))
             {
-                errorString = "Incorrect surname";
+                errorMessages.Add("Incorrect first name");
             }
-            else if ((patronymic != null) && (patronymic.Length > 0) && !CheckSubjectName(patronymic))
+            else if (!CheckStringValue(surname))
             {
-                errorString = "Incorrect patronymic";
+                errorMessages.Add("Incorrect surname");
+            }
+            else if ((patronymic != null) && (patronymic.Length > 0) && !CheckStringValue(patronymic))
+            {
+                errorMessages.Add("Incorrect patronymic");
             }
             else if ((passportNumber < MinPassportNumber) || (passportNumber > MaxPassportNumber))
             {
-                errorString = "Incorrect passport number";
+                errorMessages.Add("Incorrect passport number");
             }
-            /*
-            else if (birthday == null)
-            {
-                errorString = "Birthday didn't set";
-            }
-            */
-            if (errorString != null)
-            {
-                return false;
-            }
-            return true;
-        }
-
-        private Subject()
-        {
-
+            
+            return errorMessages.Count == 0;
         }
 
         private Subject(string name, string surname, string patronymic,
-            string passportNumberStr, string birthdayStr) : this()
+            string passportNumberStr, string birthdayStr)
         {
-            string errorString = null;
-            long passportNumber;
-            DateTime birthday;
-            if (!long.TryParse(passportNumberStr, out passportNumber))
+            var errorMessages = new List<string>(6);
+            long passportNumber = 0;
+            DateTime birthday = new DateTime();
+            try
             {
-                errorString = "Incorrect passport number"; 
+                passportNumber = long.Parse(passportNumberStr);              
             }
-            if (!DateTime.TryParse(birthdayStr, out birthday))
+            catch (System.Exception)
+            {                
+                errorMessages.Add("Invalid passport number");
+            }
+            try
             {
-                errorString = "Incorrect birth day format";
+                birthday = DateTime.Parse(birthdayStr, CultureInfo.InvariantCulture);                  
             }
-            if (errorString != null)
-            {
-                var exception = new ArgumentException();
-                exception.Data.Add("Bad input", errorString);
-                throw exception;
+            catch (System.Exception)
+            {                
+                errorMessages.Add("Invalid birth date");
             }
-            
-            string errorText = null;
-            if (CheckSubjectValues(name, surname, patronymic,
-                passportNumber, birthday, out errorText))
+            if (CheckValues(name, surname, patronymic,
+                passportNumber, birthday, ref errorMessages))
             {
                 Name = name;
                 Surname = surname;
                 Patronymic = patronymic;
                 PassportNumber = passportNumber;
                 Birthday = birthday;
+                return;
             }
-            else
+            if (errorMessages.Count != 0)
             {
                 var exception = new ArgumentException();
-                exception.Data.Add("Bad input", errorText);
+                foreach (var errorMessage in errorMessages)
+                {
+                    exception.Data.Add("Bad input", errorMessage);
+                }
                 throw exception;
             }
         }
 
-        private static bool CheckSubjectName(string value)
+        private static bool CheckStringValue(string value)
         {
             if ((value.Length < 1) || (value.Length > MaxNameLength))
             {
@@ -159,14 +156,8 @@ namespace SubjectsStorage
             }
             try
             {
-                subject = new Subject
-                {
-                    Name = fields[0],
-                    Surname = fields[1],
-                    Patronymic = fields[2],
-                    PassportNumber = Int32.Parse(fields[3]),
-                    Birthday = DateTime.Parse(fields[4], CultureInfo.InvariantCulture)
-                };
+                subject = new Subject(fields[0], fields[1], fields[2],
+                    fields[3], fields[4]);
                 return true;
             }
             catch (Exception exc)
@@ -186,7 +177,7 @@ namespace SubjectsStorage
         }
     }
     // TODO: To change ToString() method to something else
-    public static class SubjectsCsvWriter
+    public static class SubjectsToCsvConverter
     {
         public static void SaveToCsv(string path, IEnumerable<Subject> subjects)
         {
@@ -237,10 +228,10 @@ namespace SubjectsStorage
             string line = _sr.ReadLine();
             while (line != null)
             {
-                Subject subj;
-                if (Subject.TryParse(line, out subj))
+                var field = line.Split(';');
+                if ((field != null) && (field.Length == 5))
                 {
-                    resultSet.Add(subj);
+                    resultSet.Add(Subject.CreateSubject(field[0], field[1], field[2], field[3], field[4]));
                 }
                 line = _sr.ReadLine();
             }
@@ -266,29 +257,9 @@ namespace SubjectsStorage
         // TODO: Split Main() into a few methods (in switch clause at least)
         static void Main()
         {
-            try
+            var path = @"/Users/estarodubtsev/Documents/Programming/Sharp_solutions/SubjectsStorage/db.txt";
+            using (var dataStorage = new DataStorage(path))
             {
-                Console.WriteLine("1: {0}", Subject.CreateSubject("фыв", "йцу", "", 1, new DateTime()));
-                Console.WriteLine("2: {0}", Subject.CreateSubject("фыв", "йцу", "й", 1234999999, new DateTime()));
-                Console.WriteLine("3: {0}", Subject.CreateSubject("фыв", "йцу", "файацй)й", 1, new DateTime()));
-                Console.WriteLine("4: {0}", Subject.CreateSubject("фыв", "йцу", null, 1, new DateTime()));
-            }
-            catch (System.Exception exc)
-            {
-                Console.WriteLine("Error: {0}", exc.Data["Bad input"]);
-            }
-
-            return;
-            var path = @"/Users/estarodubtsev/Documents/Programming/Sharp_solutions/test2/db.txt";
-            using (var dataProvider = new DataStorage(path))
-            {
-                Console.WriteLine("db: ");
-                foreach (var subj in dataProvider.GetAllSubjects())
-                {
-                    Console.WriteLine(subj);
-                }
-                Console.WriteLine("------------");
-                
                 do
                 {
                     Console.WriteLine();
@@ -298,55 +269,24 @@ namespace SubjectsStorage
 
                     while (true)
                     {
+                        // TODO: Extract method and dictionary with commands
                         switch (inputStr)
                         {
                             case @"/exit":
                                 Console.WriteLine("Exit");
                                 return;
                             case @"/help":
-                                Console.WriteLine(@"/help - this manual");
-                                Console.WriteLine(@"/exit - close program");
-                                Console.WriteLine(@"/new  - add new subject");
-                                Console.WriteLine(@"/show - show subjects from.. to..");
+                                WriteHelp();
                                 break;
                             case @"/new":
-                                Console.WriteLine(@"Enter new subject's info separated by semicolon like this (or enter /exit):");
-                                Console.WriteLine(@"name;surname;patronymic;passport_number;dd/mm/yyyy hh:mm:ss");
-                                inputStr = Console.ReadLine();
-
-                                string name, surname, patronymic, passportNumber, birthday;
-                                Console.WriteLine(@"enter name: ");
-                                name = Console.ReadLine();
-                                Console.WriteLine(@"enter surname: ");
-                                surname = Console.ReadLine();
-                                Console.WriteLine(@"enter patronymic: ");
-                                patronymic = Console.ReadLine();
-                                Console.WriteLine(@"enter passport serial and number (without spaces): ");
-                                passportNumber = Console.ReadLine();
-                                Console.WriteLine(@"enter birth day (dd/mm/yyyy hh:mm:ss): ");
-                                birthday = Console.ReadLine();
-                                Subject nextSubj;
-                                try
-                                {
-
-                                    dataProvider.PutSubject();
-                                }
-                                catch (System.Exception exc)
-                                {
-                                    Console.WriteLine("Error: {0}", exc.Data["Bad input"]);
-                                }
-                                if (Subject.TryParse(inputStr, out nextSubj))
-                                {
-                                    dataProvider.PutSubjects(new [] { nextSubj } );
-                                    Console.WriteLine("Subject added");
-                                }
-                                else
-                                {
-                                    Console.WriteLine("There are some problems while parse string");
-                                }
+                                ReadAndSaveSubject(dataStorage);
                                 break;
                             case @"/show":
-                                Console.WriteLine("Not implemented, please type another command");
+                                foreach (var subj in dataStorage.GetAllSubjects())
+                                {
+                                    Console.WriteLine(subj);
+                                }
+                                //Console.WriteLine("Not implemented, please type another command");
                                 break;
                             default:
                                 break;
@@ -358,6 +298,38 @@ namespace SubjectsStorage
                 }
                 while (true);
             }
+        }
+
+        private static void ReadAndSaveSubject(DataStorage dataStorage)
+        {
+            string name, surname, patronymic, passportNumber, birthday;
+            Console.WriteLine(@"enter name: ");
+            name = Console.ReadLine();
+            Console.WriteLine(@"enter surname: ");
+            surname = Console.ReadLine();
+            Console.WriteLine(@"enter patronymic: ");
+            patronymic = Console.ReadLine();
+            Console.WriteLine(@"enter passport serial and number (without spaces): ");
+            passportNumber = Console.ReadLine();
+            Console.WriteLine(@"enter birth day (dd/mm/yyyy hh:mm:ss): ");
+            birthday = Console.ReadLine();
+            try
+            {
+                Subject nextSubj = Subject.CreateSubject(name, surname, patronymic, passportNumber, birthday);
+                dataStorage.PutSubject(nextSubj);
+            }
+            catch (System.Exception exc)
+            {
+                Console.WriteLine("Error: {0}", exc.Data["Bad input"]);
+            }
+        }
+
+        private static void WriteHelp()
+        {
+            Console.WriteLine(@"/help - this manual");
+            Console.WriteLine(@"/exit - close program");
+            Console.WriteLine(@"/new  - add new subject");
+            Console.WriteLine(@"/show - show subjects from.. to..");
         }
     }
 }
